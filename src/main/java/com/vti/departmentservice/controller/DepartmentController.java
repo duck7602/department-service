bfcf9ceaca1a0829e56e70f544e7b8d0cc736429
duck7602 <duc87602@gmail.com>
2026-05-28 18:23:11 +0700

    update controller

--- src/main/java/com/vti/departmentservice/controller/DepartmentController.java
+++ src/main/java/com/vti/departmentservice/controller/DepartmentController.java
@@ -16,7 +16,7 @@ import com.vti.departmentservice.service.IDepartmentService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping(value = "/api/v10/departments")
+@RequestMapping(value = "/api/v11/departments")
 @RequiredArgsConstructor
 public class DepartmentController {
     private final IDepartmentService dpService;
